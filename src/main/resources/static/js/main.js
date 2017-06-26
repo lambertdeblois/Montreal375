d35b5ccf358bd08edb3c5d54bfdf60e9a4ee61e9
Lambert <lambertdeblois@gmail.com>
2017-06-26 12:44:32 -0400

    WIP carte MTL

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,9 @@
+var mymap = L.map('carte').setView([45.52944, -73.63861], 10);
+console.log("hey");
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox.streets',
+  accessToken: 'pk.eyJ1IjoiaW5mNDM3NW1hcCIsImEiOiJjajRlYng5dWswMXR5MzNsYTJ0Y3owcnoxIn0._-7Nq-cOcGkjK1Ti6ERasA'
+}).addTo(mymap);
