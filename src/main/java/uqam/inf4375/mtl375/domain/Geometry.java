18eb39ee53071ffa4d277e8830b363ca59ad0203
Lambert D-T <lambertdt@hotmail.com>
2017-06-29 17:15:49 -0400

    tester regex

--- src/main/java/uqam/inf4375/mtl375/domain/Geometry.java
+++ src/main/java/uqam/inf4375/mtl375/domain/Geometry.java
@@ -4,15 +4,15 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Geometry {
   private String type;
-  private float[][] coordinates;
+  private float[][][] coordinates;
 
-  public Geometry(String type, float[][] coordinates) {
+  public Geometry(String type, float[][][] coordinates) {
     this.type = type;
     this.coordinates = coordinates;
   }
 
   @JsonProperty public String getType() { return type; }
-  @JsonProperty public float[][] getCoordinates() { return coordinates; }
+  @JsonProperty public float[][][] getCoordinates() { return coordinates; }
 
   @Override public String toString() {
     return String.format("%s", type); // TODO: print coordinates
