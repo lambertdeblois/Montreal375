026ef5e62a9e9f514a6b9f69f26d80372b34b09b
Lambert D-T <lambertdt@hotmail.com>
2017-06-29 17:19:27 -0400

    tester

--- src/main/java/uqam/inf4375/mtl375/domain/PisteCyclable.java
+++ src/main/java/uqam/inf4375/mtl375/domain/PisteCyclable.java
@@ -2,22 +2,22 @@ package uqam.inf4375.mtl375.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class PisteCyclable {
-  private String type;
-  private Property property;
+public class PisteCyclable {  
+  private float id;
   private Geometry geometry;
 
-  public PisteCyclable(String type, Property property, Geometry geometry) {
-    this.type = type;
-    this.property = property;
-    this.geometry = geometry;
-  }
+    public PisteCyclable() {
+    }  
 
-  @JsonProperty public String getType() { return type; }
-  @JsonProperty public Property getProperty() { return property; }
+  public PisteCyclable(float id, Geometry geometry) {
+    
+    this.id = id;
+    this.geometry = geometry;
+  }  
+  
   @JsonProperty public Geometry getGeometry() { return geometry; }
 
   @Override public String toString() {
-    return String.format("%s", type); // TODO: print avec geometry et property
+    return String.format("%.6f", id); // TODO: print avec geometry 
   }
 }
